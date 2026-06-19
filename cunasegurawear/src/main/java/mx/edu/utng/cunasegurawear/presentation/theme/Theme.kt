b7c98fcfa937e84c1d3794c74db0af4d6c6e7703
gus-p3 <sinnamexd3432@gmail.com>
2026-06-19 13:53:53 -0600

    first commit

---
+++ cunasegurawear/src/main/java/mx/edu/utng/cunasegurawear/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package mx.edu.utng.cunasegurawear.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material3.MaterialTheme
+
+@Composable
+fun CunaSeguraTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
