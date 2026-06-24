43a89dfe33a1b9c2ca2a42dd502471d747ee9b68
gus-p3 <sinnamexd3432@gmail.com>
2026-06-23 23:09:24 -0600

    fix(wear): use valid hexadecimal values in BleProtocol UUIDs

--- cunasegurawear/src/main/java/mx/edu/utng/cunasegurawear/data/ble/BleProtocol.kt
+++ cunasegurawear/src/main/java/mx/edu/utng/cunasegurawear/data/ble/BleProtocol.kt
@@ -3,8 +3,8 @@ package mx.edu.utng.cunasegurawear.data.ble
 import java.util.UUID
 
 object BleProtocol {
-    val SERVICE_UUID: UUID     = UUID.fromString("0000cuna-0000-1000-8000-00805f9b34fb")
-    val SOS_TRIGGER_UUID: UUID = UUID.fromString("0000sos0-0000-1000-8000-00805f9b34fb")
-    val SOS_CANCEL_UUID: UUID  = UUID.fromString("0000canc-0000-1000-8000-00805f9b34fb")
-    val CONFIG_READ_UUID: UUID = UUID.fromString("0000conf-0000-1000-8000-00805f9b34fb")
+    val SERVICE_UUID: UUID     = UUID.fromString("0000c11a-0000-1000-8000-00805f9b34fb")
+    val SOS_TRIGGER_UUID: UUID = UUID.fromString("00005050-0000-1000-8000-00805f9b34fb")
+    val SOS_CANCEL_UUID: UUID  = UUID.fromString("0000ca1c-0000-1000-8000-00805f9b34fb")
+    val CONFIG_READ_UUID: UUID = UUID.fromString("0000c01f-0000-1000-8000-00805f9b34fb")
 }
